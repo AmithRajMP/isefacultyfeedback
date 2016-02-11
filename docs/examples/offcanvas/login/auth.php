1325c08e0eec79fdb55299e08a244e1d4830948b
AmithRajMP <amith.veyron@gmail.com>
2016-02-12 00:23:05 +0530

    First commit

---
+++ docs/examples/offcanvas/login/auth.php
@@ -0,0 +1,107 @@
+<?php 
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$username=$_POST['username'];
+$password=$_POST['password'];
+
+if($username =="vaidehi" && $password =="vaidehimam")
+{
+	header("location: https://docs.google.com/spreadsheets/d/1LaMji4iqaLTR6UEuKDegjYWjLJd3qPCO5h5LAavHGYo/pubhtml");
+	exit();
+}
+elseif ($username =="suma" && $password == "sumamam") {
+	header("location: https://docs.google.com/spreadsheets/d/12zTRNTq3T_EQKy9fPL49URWTNvhRqVP5HPhYsyKoItI/pubhtml");
+	exit();
+}
+elseif ($username =="bindu" && $password == "bindumam") {
+	header("location: https://docs.google.com/spreadsheets/d/1sottsbEf1sNQNHjx00cZkJfvFonnHtSe3EvX94xz8oc/pubhtml");
+	exit();
+}
+elseif ($username =="kodhanpur" && $password == "kodhanpursir") {
+	header("location: https://docs.google.com/spreadsheets/d/1clakJCToKLkbQ4SLrQ7J_7KZT3lRkU673BeK1LpEkV4/pubhtml");
+	exit();
+}
+elseif ($username =="sunanda" && $password == "sunandamam") {
+	header("location: https://docs.google.com/spreadsheets/d/1itAqSO7yLYeaMC5a4Z30l20_XjYbhdAgXlMhkSoqyvc/pubhtml");
+	exit();
+}
+elseif ($username =="kanagavalli" && $password == "kanagavallimam") {
+	header("location: https://docs.google.com/spreadsheets/d/1Li9mifJtNHuUEuhSIflVnCcMzQOeO34hjtkdGog7ohU/pubhtml");
+	exit();
+}
+elseif ($username =="rajeshwari" && $password == "rajeshwarimam") {
+	header("location: https://docs.google.com/spreadsheets/d/1kN5TxaMMayrEp13ztEiJYN_U2hHV3XzTtxxCUQl1J-4/pubhtml");
+	exit();
+}
+elseif ($username =="muzameel" && $password == "muzameelsir") {
+	header("location: https://docs.google.com/spreadsheets/d/1lf_9W9ZEmt_31g-ExeqSEgaxOXS6Lgi-ZlKiPdynvpk/pubhtml");
+	exit();
+}
+elseif ($username =="chandrakala" && $password == "chandrakalamam") {
+	header("location: https://docs.google.com/spreadsheets/d/1K4BXrLwwqWySvpjR7x3ZrLd3lar22fHTy7Fgv8ZMJ-Q/pubhtml");
+	exit();
+}
+elseif ($username =="suresh" && $password == "sureshsir") {
+	header("location: https://docs.google.com/spreadsheets/d/1aRC9tuom43J51W7tC-Xgt5GMmaK9bMNPAUIOOn20zZc/pubhtml");
+	exit();
+}
+elseif ($username =="bhavani" && $password == "bhavanimam") {
+	header("location: https://docs.google.com/spreadsheets/d/1R1-82ElkpDugvcPXDgXIGv17OiJBH5t0yf-BvEA-f_M/pubhtml");
+	exit();
+}
+elseif ($username =="latha" && $password == "lathamam") {
+	header("location: https://docs.google.com/spreadsheets/d/1sH2oWrYx2RMENKRehMada-WjyuOUZfiwDdKswAGDt0E/pubhtml");
+	exit();
+}
+elseif ($username =="vani" && $password == "vanimam") {
+	header("location: https://docs.google.com/spreadsheets/d/1Jwmr5YK5Um72EiqU2exKnQjjOxUrm-T2plch4oaweFY/pubhtml");
+	exit();
+}
+elseif ($username =="prathima" && $password == "prathimamam") {
+	header("location: https://docs.google.com/spreadsheets/d/1vKSYwP7SpbyfqEPE0YRJhlD3NFlt6vrR71jxIxBUOHo/pubhtml");
+	exit();
+}
+elseif ($username =="seema" && $password == "seemamam") {
+	header("location: https://docs.google.com/spreadsheets/d/14HqONQ-ByQNVB8Onh6zOXfwKtjoFKG3Grp7HIGyJAiA/pubhtml");
+	exit();
+}
+elseif ($username =="rekha" && $password == "rekhamam") {
+	header("location: https://docs.google.com/spreadsheets/d/106k7qFq3BofZIQUQDwibV-sqVAwvOD0GF5C-jQIusrE/pubhtml");
+	exit();
+}
+elseif ($username =="radhika" && $password == "radhikamam") {
+	header("location: https://docs.google.com/spreadsheets/d/1I-5A8gdQZagaZCrcks3MKFtepULxc8vgWkF2uP7dJZ0/pubhtml");
+	exit();
+}
+elseif ($username =="namitha" && $password == "namithamam") {
+	header("location: https://docs.google.com/spreadsheets/d/1QcormMkV5e35HNHo_mbsj4OMzxzeH4pmQzQ4me0q_H8/pubhtml");
+	exit();
+}
+elseif ($username =="krupa" && $password == "krupamam") {
+	header("location: https://docs.google.com/spreadsheets/d/1D6pIjUQunQbXOZH99IMAu-cZ2_IsW4EZ9IrNnAOm4tM/pubhtml");
+	exit();
+}
+elseif ($username =="sharon" && $password == "sharonmam") {
+	header("location: https://docs.google.com/spreadsheets/d/15d9j-P3IQG4mr6rbQ6fBwqc8CwclfoqXjowJs8j7hQU/pubhtml");
+	exit();
+}
+elseif ($username =="shalini" && $password == "shalinimam") {
+	header("location: https://docs.google.com/spreadsheets/d/1XkuKUJsSv7rnw1AevJOfXjRCfbi0UAAqiXDGfUA4ehk/pubhtml");
+	exit();
+}
+elseif ($username =="shilpa" && $password == "shilpamam") {
+	header("location: https://docs.google.com/spreadsheets/d/1ooDo7cxYcCCfdikzfjXe0JDfaxkhtoMgZI4tO1PTeLM/pubhtml");
+	exit();
+}
+elseif ($username =="jasmine" && $password == "jasminemam") {
+	header("location: https://docs.google.com/spreadsheets/d/1B43Sr2H0RR7mhmD0uqHxUBw-auD3VwTFX1Zhtgre8fo/pubhtml");
+	exit();
+}
+elseif ($username =="dheemanth" && $password == "dheemanthsir") {
+	header("location: https://docs.google.com/spreadsheets/d/1Hqj3OaiQ701-mA0Ms1LzzkZv-lkHDiE33xjvKzlKgxE/pubhtml");
+	exit();
+}
+
+?>
---
+++ docs/examples/signin/auth.php
@@ -0,0 +1,12 @@
+<?php 
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+if($username = "demo@gmail.com" && $password = "demo")
+{
+	header("location: https://docs.google.com/spreadsheets/d/1LaMji4iqaLTR6UEuKDegjYWjLJd3qPCO5h5LAavHGYo/pubhtml");
+}
+
+?>
